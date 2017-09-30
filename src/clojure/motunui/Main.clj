1f699c94dcb0afa5d195b617511b26ce7f58bace
Rolf C Stadheim <rolf.c.stadheim@gmail.com>
2017-09-30 16:48:20 +0200

    Write-out to R

--- src/clojure/motunui/Main.clj
+++ src/clojure/motunui/Main.clj
@@ -22,6 +22,8 @@
 
 (def calc-cc-10 (comp (CyberCycle. 10) (SuperSmoother. 10)))
 
+(def calc-cc-10r (comp (CyberCycle. 10) (RoofingFilter.)))
+
 (def min-dx (LocalDate/of 2014 1 1))
 
 (def min-sql-date (Date/valueOf ^LocalDate min-dx))
@@ -30,17 +32,26 @@
   (DB/with-session "mybatis.conf.xml" StockMapper
     (.selectStockPrices ^StockMapper it oid min-sql-date)))
 
-(defn normalize-dates [dx]
-  (map #(COM/diff-days min-dx %) dx))
+;(defn write-file []
+;  (with-open [w (clojure.java.io/writer  "../R/w.txt" :append true)]
+;    (.write w (str "hello" "world"))))
 
-(defn to-R [prices]
-  (let [spots (map #(.getCls ^StockPriceBean %) prices)
-        dx (map #(.toLocalDate ^Date (.getDx ^StockPriceBean %)) prices)
-        cc (calc-cc-10 spots)
-        ndx (normalize-dates dx)]
-    (COM/process-lists-with println ndx cc)))
+(def normalize-dates (partial COM/normalize-dates min-dx)) 
+
+(defn to-R [oid prices num-items]
+  (with-open [w (clojure.java.io/writer  (str "../R/" oid ".txt"))]
+    (let [
+          spots (map #(.getCls ^StockPriceBean %) prices)
+          num-drop (- (.size spots) num-items)
+          dx (map #(.toLocalDate ^Date (.getDx ^StockPriceBean %)) prices)
+          cc (COM/normalize (drop num-drop (calc-cc-10 spots)))
+          ccr (COM/normalize (drop num-drop (calc-cc-10r spots)))
+          ndx (normalize-dates (drop num-drop dx))]
+      (.write w (str "n\tcc\tccr\n"))
+      (COM/process-lists-with #(.write w (str %1 "\t" %2 "\t" %3 "\n")) ndx cc ccr))))
 
 (defn -main [& args]
   (let [oid (Integer/parseInt (first args))
+        ni (Integer/parseInt (first (rest args)))
         px (prices oid)]
-    (to-R px)))
+    (to-R oid px ni)))
