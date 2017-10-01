8a057c4fb1e659169e0046266c2b0395e19f84e5
Rolf C Stadheim <rolf.c.stadheim@gmail.com>
2017-10-01 18:00:03 +0200

    https

--- src/python/to_r.py
+++ src/python/to_r.py
@@ -3,11 +3,12 @@
 import requests
 import json
 
-url = "http://localhost:8082/maunaloa/to_r?oid=%s"
+# url = "http://localhost:8082/maunaloa/to_r?oid=%s"
+url = "https://hilo/maunaloa/to_r?oid=%s"
 
 oid = "3"
 
-r = requests.get(url % oid)
+r = requests.get(url % oid, verify=False)
 
 j = json.loads(r.text)
 
@@ -16,9 +17,14 @@ cc = j['cc']
 cc_rf = j['cc_rf']
 cc_ss = j['cc_ss']
 
+
+for x in cc_rf: print (x)
+
+"""
 with open("../../R/%s.txt" % oid,'w') as f:
     f.write("n\tcc\tcc_ss\tcc_rf\n")
     for x in zip(ndx,cc,cc_ss,cc_rf):
         f.write("%d\t%.15f\t%.15f\t%.15f\n" % (x[0],x[1],x[2],x[3]))
+"""
 
 
