2558cd4bc03be3085aabf585990032385733ddf9
Rolf C Stadheim <rolf.c.stadheim@gmail.com>
2017-10-02 14:03:11 +0200

    to_r with weeks

--- src/python/to_r.py
+++ src/python/to_r.py
@@ -4,7 +4,8 @@ import requests
 import json
 
 # url = "http://localhost:8082/maunaloa/to_r?oid=%s"
-url = "https://hilo/maunaloa/to_r?oid=%s"
+# url = "https://hilo/maunaloa/to_r?oid=%s"
+url = "https://andromeda/maunaloa/to_r?oid=%s"
 
 oid = "3"
 
@@ -12,19 +13,25 @@ r = requests.get(url % oid, verify=False)
 
 j = json.loads(r.text)
 
-ndx = j['ndx']
-cc = j['cc']
-cc_rf = j['cc_rf']
-cc_ss = j['cc_ss']
+def write_to_R(is_daily):
+    if is_daily == True:
+        ndx = j['ndx']
+        cc = j['cc']
+        cc_rf = j['cc_rf']
+        cc_ss = j['cc_ss']
+        fname = "../../R/%s.txt" % oid
+    else:
+        ndx = j['w_ndx']
+        cc = j['w_cc']
+        cc_rf = j['w_cc_rf']
+        cc_ss = j['w_cc_ss']
+        fname = "../../R/%s_w.txt" % oid
+    with open(fname,'w') as f:
+        f.write("n\tcc\tcc_ss\tcc_rf\n")
+        for x in zip(ndx,cc,cc_ss,cc_rf):
+            f.write("%d\t%.15f\t%.15f\t%.15f\n" % (x[0],x[1],x[2],x[3]))
 
-
-for x in cc_rf: print (x)
-
-"""
-with open("../../R/%s.txt" % oid,'w') as f:
-    f.write("n\tcc\tcc_ss\tcc_rf\n")
-    for x in zip(ndx,cc,cc_ss,cc_rf):
-        f.write("%d\t%.15f\t%.15f\t%.15f\n" % (x[0],x[1],x[2],x[3]))
-"""
+write_to_R(True)
+write_to_R(False)
 
 
